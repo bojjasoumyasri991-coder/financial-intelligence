fa2df9b0ecbd02579ab01d983a986a71f7b05919
Souma sri <bojjasoumyasri991@gmail.com>
2026-06-05 15:16:17 +0530

    Enable debug

--- api/settings.py
+++ api/settings.py
@@ -76,19 +76,13 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'financial-intelligence',
-        'USER': 'postgres',
-        'PASSWORD': 'mysql123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+import os
+import dj_database_url
 
-        'OPTIONS': {
-            'options': '-c search_path=warehouse'
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
